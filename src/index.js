1302d97ee2768264b16243488a7b2c621b595aff
darkknight13 <19567498+darkknight13@users.noreply.github.com>
2024-06-07 12:40:09 +0530

    export added

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export { StoreBase } from './StoreBase.js';
 export { StoreIN1 } from './StoreIN1.js';
 export { StorePID } from './StorePID.js';
 export { StorePV1 } from './StorePV1.js';
+export { StoreAISGPL } from './StoreAISGPL.js';
